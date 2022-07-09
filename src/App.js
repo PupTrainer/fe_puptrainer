b58d2a18cc4ec178a0c565fee188acb45349af58
Nicholas Ao <aominhlong@yahoo.com>
2022-07-09 12:36:02 -0600

    Add switch and error handling for bad url

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import Homepage from './Homepage'
 import Login from './Login'
 import About from './About'
 
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 const App = () => {
 
 
 return (
   <div>
     <h2>APP</h2>
-    <Route exact path='/'>
-      <Login />
-    </Route>
-    <Route path='/homepage'>
-      < Nav />
-      < Homepage />
-    </Route>
-    <Route path='/about'>
-      <About />
-    </Route>
+    <Switch>
+      <Route exact path='/'>
+        <Login />
+      </Route>
+      <Route path='/homepage'>
+        < Nav />
+        < Homepage />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route render={() => {
+        <h1>Nothing here. Go back Home.</h1>
+      }} />
+    </Switch>
   </div>
 )
 }

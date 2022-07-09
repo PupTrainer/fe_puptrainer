36a4290bb150750d176ceae4944fa3f171fe3126
Josh Mallery <josh.mallery@gmail.com>
2022-07-09 17:53:32 -0600

    Add Nav Sass file

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import "./Nav.css";
 const Nav = () => {
     return (
-        <div>
+        <div className='links'>
             <NavLink to='/User'>User Profile</NavLink>
             <NavLink to='/About'>About</NavLink>
             <NavLink to='/Login'>Logout</NavLink>

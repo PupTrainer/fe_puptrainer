12dfe2e78734f018fc36c7791974e3af03a07fde
Nicholas Ao <aominhlong@yahoo.com>
2022-07-09 19:14:08 -0600

    Add basic styling to homepage and revert App to original file

--- src/App.js
+++ src/App.js
@@ -8,26 +8,26 @@ import { Route, Switch } from 'react-router-dom';
 const App = () => {
 
 
-return (
-  <div>
-    <h2>APP</h2>
-    <Switch>
-      <Route exact path='/'>
-        <Login />
-      </Route>
-      <Route path='/homepage'>
-        < Nav />
-        < Homepage />
-      </Route>
-      <Route path='/about'>
-        <About />
-      </Route>
-      <Route render={() => {
-        <h1>Nothing here. Go back Home.</h1>
-      }} />
-    </Switch>
-  </div>
-)
-}
+  return (
+    <div>
+      <h2>APP</h2>
+      <Switch>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+        <Route path='/homepage'>
+          < Nav />
+          < Homepage />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route render={() => {
+          <h1>Nothing here. Go back Home.</h1>
+        }} />
+      </Switch>
+    </div>
+  )
+  }
 
 export default App

2b3ccf4090c7faa736af94ca2b49a34d2ff76152
Daniel Neer <daniel.r.neer@gmail.com>
2022-07-19 15:23:26 -0700

    Add skills styling

--- src/Skill.js
+++ src/Skill.js
@@ -4,22 +4,21 @@ import './Skill.css'
 const Skill = ({ name, id, description, level, criteria, youtubeLink }) => {
   console.log(description)
   return (
-    <div>
-      <div>
+    <div className='skill-container'>
         <div className='description'>
-          <h2>Skill Description</h2>
-          <p>{name}</p>
-          <p>Level: {level}</p>
-          <p>{description}</p>
+            <h2 className='skill-title'>Skill Description</h2>
+            <p className='skill-name'>{name}</p>
+            <p>Level: {level}</p>
+            <p>{description}</p>
+            <div>
+              <p className='requirement'>Pass Requirements:</p>
+              <p>{criteria}</p>
+            </div>
         </div>
-        <div>
-        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${youtubeLink}`} title="YouTube video player" frameBorder="0"></iframe>
-        </div>
-        <div>
-          <p>Pass Requirements:<br></br>{criteria}</p>
-        </div>
-        <button>Complete Training</button>
-      </div>
+          <div>
+            <iframe className='video' width="560" height="315" src={`https://www.youtube.com/embed/${youtubeLink}`} title="YouTube video player" frameBorder="0"></iframe>
+            <button className='training-btn'>Complete Training</button>
+          </div>
     </div>
   )
 }
